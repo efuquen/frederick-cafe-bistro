13f50cad700b8140e3ca29f09aa889c4b5657cd7
Edwin Fuquen <ed@grooveshark.com>
2012-09-08 15:45:46 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FrederickCafeBistro::Application.config.secret_token = '14e3dc3968d0a9bdcca4bbc0bf02b3005e16199a96c963abf266aa021bb70e9f28aa3445dcc9312c7fffe086dcf4197b336ad986020f1d02cb664ed0671674b7'
